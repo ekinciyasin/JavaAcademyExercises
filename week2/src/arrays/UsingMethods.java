68584d06dcae29eec03584d82e7b8cc471bf1cb3
Yasin Ekinci <yasin.ekinci@hotmail.com>
2024-04-18 11:49:47 +0200

    recreate usingMethods class

--- week2/src/arrays/UsingMethods.java
+++ week2/src/arrays/UsingMethods.java
@@ -4,21 +4,22 @@ public class UsingMethods {
     public static void main(String[] args) {
         int[] numbers = new int[] {2, 2, 10, 11, 10};
         int multiplier = 2;
+        int [] multipliedArray = multiplyArray(numbers, multiplier);
         printMultipliedArray(numbers, multiplier);
 
     }
 
-    public static void printMultipliedArray(int[] numbers, int multiplier) {
+    private static int[] multiplyArray(int[] numbers, int multiplier) {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] *= multiplier;
+        }
+        return numbers;
+    }
 
+    public static void printMultipliedArray(int[] numbers, int multiplier) {
             System.out.println("Numbers array after multiplication:");
             for (int num : numbers) {
                 System.out.print(num + " ");
             }
-            System.out.println();
-
-            System.out.println("Multiplier: " + multiplier);
-        }
     }
 }
